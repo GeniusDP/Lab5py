d03f7399554dc007cfdab23d850b3601760ad5b0
Zaranik Bogdan <zaranikBZ@gmail.com>
2020-11-25 11:04:55 +0200

    Добавил комментарии.

--- Lab5py.py
+++ Lab5py.py
@@ -1,9 +1,12 @@
 from math import sqrt
+#ввод числа N
 n=int(input("Input none-negative integer number n: "))
+#перебор комбинаций чисел a<=b<=c<=d
 for a in range(0, int(sqrt(n))+1):
     for b in range(a, int(sqrt(n))+1):
         for c in range(b, int(sqrt(n))+1):
             for d in range(c, int(sqrt(n))+1):
+                #Если комбинация подошла, выводим её
                 if( a*a + b*b + c*c + d*d == n ):
                     print("%4d"%a, end="")
                     print("%4d"%b, end="")
